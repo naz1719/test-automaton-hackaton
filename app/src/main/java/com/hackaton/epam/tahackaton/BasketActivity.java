52305d0640efde4a23cd0418d3a9949430f2e703
Volodymyr_Lominskyi <SoftSmyJob1234>
2017-10-29 11:05:48 +0200

    Basket functionality has been implemented

--- app/src/main/java/com/hackaton/epam/tahackaton/BasketActivity.java
+++ app/src/main/java/com/hackaton/epam/tahackaton/BasketActivity.java
@@ -1,20 +1,27 @@
 package com.hackaton.epam.tahackaton;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class BasketActivity extends AppCompatActivity {
 
-    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basket);
-        TextView userName = findViewById(R.id.username);
-        userName.setText("BASKET");
+
+        Button b = findViewById(R.id.basketBtn);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cart = new Intent(BasketActivity.this, CartActivity.class);
+                startActivity(cart);
+            }
+        });
     }
 }
